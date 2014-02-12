47514b8647d7b7a6c839971ef96d55651fe16afe
leandrafinger <leandra.finger@gmail.com>
2014-02-12 15:19:05 +0100

    footer navigation version 2

---
+++ allink_essentials/in_footer/page_extension_v2.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from feincms import extensions
+
+
+class Extension(extensions.Extension):
+    def handle_model(self):
+
+        self.model.add_to_class('in_footer', models.BooleanField(_('In footer'), default=False, help_text=_('This displays the page in the footer navigation.')))
+
+    def handle_modeladmin(self, modeladmin):
+        modeladmin.list_display.extend(['in_footer'])
+        modeladmin.list_editable.extend(['in_footer'])
+        modeladmin.add_extension_options(_('Footer Navigation'), {
+            'fields': ('in_footer',),
+        })
