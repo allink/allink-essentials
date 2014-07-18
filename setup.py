e8c13e5b2689f61e5943b2d4dac1449ccc8758b5
leandrafinger <leandra.finger@gmail.com>
2014-07-18 15:33:21 +0200

    allink-essentials to pypi

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Communications :: Email',
     ],
