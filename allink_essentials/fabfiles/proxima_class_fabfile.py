912026e599dce37598a219395cab83caba536bfa
leandrafinger <leandra.finger@gmail.com>
2016-03-14 08:23:00 +0100

    fixed allink essentials

--- allink_essentials/fabfiles/proxima_class_fabfile.py
+++ allink_essentials/fabfiles/proxima_class_fabfile.py
@@ -196,6 +196,7 @@ def _update_requirements_remote():
         if virtualenv_existed:
             # virtualenv exists, build wheels for the new requirements
             with prefix('source env/bin/activate'):
+                run('pip install wheel')
                 run('pip wheel --requirement REQUIREMENTS_SERVER')
             run('rm -r env_old', quiet=True)
             run('mv env env_old')
@@ -203,7 +204,6 @@ def _update_requirements_remote():
         # create new virtualenv
         run('virtualenv env --prompt="(%s)"' % env.project)
         with prefix('source env/bin/activate'):
-            run('pip install wheel')
             run('pip install %s --requirement REQUIREMENTS_SERVER' % ('--no-index' if virtualenv_existed else '',))
         # create new hash file
         run('sha1sum --tag REQUIREMENTS REQUIREMENTS_SERVER > REQUIREMENTS.sha1')
