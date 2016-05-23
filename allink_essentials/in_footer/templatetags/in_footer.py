c2ef033ae34bac38e8fb0f93f5df5bd8127d5a88
leandrafinger <leandra.finger@gmail.com>
2016-05-23 15:09:51 +0200

    commented

--- allink_essentials/in_footer/templatetags/in_footer.py
+++ allink_essentials/in_footer/templatetags/in_footer.py
@@ -10,6 +10,7 @@ register = template.Library()
 
 class FooterNavigationNode(SimpleAssignmentNodeWithVarAndArgs):
     """
+    DEPRICATED: USE footer_pages TAG
     {% load in_footer %}
     {% footer_navigation for feincms_page as footer_pages %}
     {% for p in footer_pages %}
@@ -25,5 +26,14 @@ register.tag('footer_navigation', do_simple_assignment_node_with_var_and_args_he
 
 @register.simple_tag(name="footer_pages")
 def in_footer():
+    """
+    {% load in_footer %}
+    {% footer_pages as footer_pages %}
+    {% for p in footer_pages %}
+        <li>
+            <a href="{{ p.get_absolute_url }}">{{ p.title }}</a>
+        </li>
+    {% endfor %}
+    """
     queryset = Page.objects.active().filter(in_footer=True)
     return queryset.filter(language=translation.get_language()) if 'language' in [f.name for f in Page._meta.fields] else queryset
